f088e23906416cdd9b5e76e5f3977e43ea0f6e70
lucaskja <lucaskja@usp.br>
2022-10-24 15:44:34 -0300

    Tradução e início

---
+++ build/CMakeFiles/ep.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucaskja/Documents/EACH_COMP/MVGA/TrackBall.cpp" "CMakeFiles/ep.dir/TrackBall.cpp.o" "gcc" "CMakeFiles/ep.dir/TrackBall.cpp.o.d"
+  "/home/lucaskja/Documents/EACH_COMP/MVGA/main.cpp" "CMakeFiles/ep.dir/main.cpp.o" "gcc" "CMakeFiles/ep.dir/main.cpp.o.d"
+  "/home/lucaskja/Documents/EACH_COMP/MVGA/scrInteractor.cpp" "CMakeFiles/ep.dir/scrInteractor.cpp.o" "gcc" "CMakeFiles/ep.dir/scrInteractor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ep.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ep.dir/TrackBall.cpp.o"
+  "CMakeFiles/ep.dir/TrackBall.cpp.o.d"
+  "CMakeFiles/ep.dir/main.cpp.o"
+  "CMakeFiles/ep.dir/main.cpp.o.d"
+  "CMakeFiles/ep.dir/scrInteractor.cpp.o"
+  "CMakeFiles/ep.dir/scrInteractor.cpp.o.d"
+  "ep"
+  "ep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
